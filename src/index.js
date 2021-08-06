66b3db67d13df84444fb472fde2df07b6142a33b
Melissa Rice <melissa.rice@perficient.com>
2021-08-06 15:25:43 -0400

    sent CommentDetail component as a prop to ApprovalCard component

--- src/index.js
+++ src/index.js
@@ -2,28 +2,45 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:00PM"
-        content="Nice Post!"
-        profilePic={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00PM"
-        content="Great, thanks for sharing!"
-        profilePic={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 5:00PM"
-        content="Awesome!"
-        profilePic={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        {/* taking CommentDetail component and passing it as a prop down into the ApprovalCard */}
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:00PM"
+          content="Nice Post!"
+          profilePic={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00PM"
+          content="Great, thanks for sharing!"
+          profilePic={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          content="Awesome!"
+          profilePic={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
